a5a979d42e02b2d79ea65cef7f8d13be5c0e4d98
zavr-1 <zavr-1@users.noreply.github.com>
2019-12-05 23:50:05 +0300

    Initial commit

---
+++ compile/template.js
@@ -0,0 +1,12 @@
+const { _myNewPackage } = require('./mnp')
+
+/**
+ * @methodType {_myNewPackage.myNewPackage}
+ */
+function myNewPackge(config) {
+  return _myNewPackage(config)
+}
+
+module.exports = myNewPackge
+
+/* typal types/index.xml namespace */
